9e10360395a1919717f93427bb88df8e21e083f7
Steven Ngo <sevngo@gmail.com>
2019-04-25 14:08:46 +0200

    create withGeolocation hoc

---
+++ client/hoc/withGeolocation.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { isNil } from 'ramda';
+
+const withGeolocation = fieldName => Component => {
+  const EnhancedComponent = props => {
+    const { setFieldValue, values } = props;
+    useEffect(() => {
+      if (values[fieldName]) {
+        navigator.geolocation.getCurrentPosition(({ coords: { longitude, latitude } }) => {
+          /*global google*/
+          const geocoder = new google.maps.Geocoder();
+          geocoder.geocode(
+            { location: { lat: latitude, lng: longitude } },
+            ([{ formatted_address }]) => {
+              const address = {
+                name: formatted_address,
+                type: 'Point',
+                coordinates: [longitude, latitude],
+              };
+              setFieldValue(fieldName, address);
+            },
+          );
+        });
+      }
+    }, [isNil(values[fieldName])]);
+    return <Component {...props} />;
+  };
+  return EnhancedComponent;
+};
+
+export default withGeolocation;
