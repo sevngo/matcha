424d9070467ee885fbf1945578e90e5bafdcc870
Steven Ngo <sevngo@gmail.com>
2019-04-26 16:37:34 +0200

    add geolocalisation by IP and favicon

--- client/hoc/withGeolocation.js
+++ client/hoc/withGeolocation.js
@@ -1,26 +1,39 @@
 import React, { useEffect } from 'react';
 import { isNil } from 'ramda';
+import axios from 'axios';
 
 const withGeolocation = fieldName => Component => {
   const EnhancedComponent = props => {
     const { setFieldValue, values, withGeolocation } = props;
     useEffect(() => {
       if (values[fieldName] && withGeolocation) {
-        navigator.geolocation.getCurrentPosition(({ coords: { longitude, latitude } }) => {
+        navigator.geolocation.getCurrentPosition(
+          ({ coords: { longitude: lng, latitude: lat } }) => {
+            setField(lng, lat);
+          },
+          async () => {
+            const {
+              data: {
+                location: { lat, lng },
+              },
+            } = await axios.post(
+              'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyAn3j5nVI5FQI9Av1sciWATJIFT9l-fvvw',
+            );
+            setField(lng, lat);
+          },
+        );
+        const setField = (lng, lat) => {
           /*global google*/
           const geocoder = new google.maps.Geocoder();
-          geocoder.geocode(
-            { location: { lat: latitude, lng: longitude } },
-            ([{ formatted_address }]) => {
-              const address = {
-                name: formatted_address,
-                type: 'Point',
-                coordinates: [longitude, latitude],
-              };
-              setFieldValue(fieldName, address);
-            },
-          );
-        });
+          geocoder.geocode({ location: { lat, lng } }, ([{ formatted_address }]) => {
+            const address = {
+              name: formatted_address,
+              type: 'Point',
+              coordinates: [lng, lat],
+            };
+            setFieldValue(fieldName, address);
+          });
+        };
       }
     }, [isNil(values[fieldName])]);
     return <Component {...props} />;
