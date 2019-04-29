2caba1cd03dd1bcbe1ba9c9099d8337c8f1d2eca
Steven Ngo <sngo@europ-assistance.com>
2019-04-29 17:36:34 +0200

    add block and unblock user

---
+++ client/containers/User/messages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  blockUser: {
+    id: 'containers.user.blockUser',
+    defaultMessage: 'Block user',
+  },
+  unblockUser: {
+    id: 'containers.user.unblockUser',
+    defaultMessage: 'Unblock user',
+  },
+});
