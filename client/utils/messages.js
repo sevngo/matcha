aebcbfd82ea52263a0f490ab496b0e176419909e
Steven Ngo <sevngo@gmail.com>
2019-05-01 18:09:17 +0200

    update translations

---
+++ client/utils/messages.js
@@ -0,0 +1,28 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  required: {
+    id: 'utils.validates.required',
+    defaultMessage: 'Required',
+  },
+  invalid: {
+    id: 'utils.validates.invalid',
+    defaultMessage: 'Invalid',
+  },
+  tooShort: {
+    id: 'utils.validates.tooShort',
+    defaultMessage: 'Too short',
+  },
+  tooLong: {
+    id: 'utils.validates.tooLong',
+    defaultMessage: 'Too long',
+  },
+  tooOld: {
+    id: 'utils.validates.tooOld',
+    defaultMessage: 'Too old',
+  },
+  tooYoung: {
+    id: 'utils.validates.tooYoung',
+    defaultMessage: 'Too young',
+  },
+});
