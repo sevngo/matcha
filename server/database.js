38073e5cd3d8114a95bba3ec8033b85ad59f9a93
Steven Ngo <sevngo@gmail.com>
2019-05-04 01:04:53 +0200

    install jest, supertest and create first test

--- server/database.js
+++ server/database.js
@@ -4,14 +4,17 @@ const { usersModel, USERS } = require('./models/users');
 const url = process.env.MONGODB_URL;
 const databaseName = process.env.DATABASE_NAME;
 let db;
+let client;
 
 const connectDb = async () => {
-  const client = await MongoClient.connect(url, { useNewUrlParser: true });
+  client = await MongoClient.connect(url, { useNewUrlParser: true });
   db = client.db(databaseName);
   console.log('Database connected'); // eslint-disable-line no-console
   return await usersModel(db);
 };
 
+const disconnectDb = async () => await client.close();
+
 const Users = () => db.collection(USERS);
 
-module.exports = { connectDb, Users };
+module.exports = { connectDb, Users, disconnectDb };
