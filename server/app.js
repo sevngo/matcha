90f1f3ccdde3d27738b8964bcd9d2f1d5b3ef02f
Steven Ngo <sngo@MacBook-Pro-de-Steven.local>
2019-05-04 22:56:32 +0200

    create post images test

--- server/app.js
+++ server/app.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const path = require('path');
-const usersRouter = require('./routers/users');
 const morgan = require('morgan');
+const usersRouter = require('./routers/users');
+const { NODE_ENV, DEVELOPMENT, PRODUCTION } = require('./utils/constants');
 
 const app = express();
 
-app.use(morgan('dev'));
+if (NODE_ENV === DEVELOPMENT) app.use(morgan('dev'));
+
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
 
-app.use(express.static('dist'));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'dist', 'index.html')));
+if (NODE_ENV === PRODUCTION) {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'dist', 'index.html')));
+}
 
 module.exports = app;
