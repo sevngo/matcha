08286019120e9cc76d200da4386be824e7971160
Steven Ngo <sngo@MacBook-Pro-de-Steven.local>
2019-05-04 19:32:19 +0200

    create data file for tests

--- server/tests/users.test.js
+++ server/tests/users.test.js
@@ -1,30 +1,9 @@
 /* eslint-env node, jest */
 const request = require('supertest');
-const { ObjectID } = require('mongodb');
-const bcrypt = require('bcryptjs');
-const faker = require('faker');
 const app = require('../app');
 const { connectDb, disconnectDb } = require('../database');
 const { Users } = require('../database');
-
-const password = faker.internet.password();
-const initialUser = {
-  username: faker.internet.userName(),
-  birthDate: faker.date.past(),
-  firstName: faker.name.firstName(),
-  lastName: faker.name.lastName(),
-  email: faker.internet.email(),
-  gender: 'male',
-  address: {
-    type: 'Point',
-    name: faker.address.streetAddress(),
-    coordinates: [parseFloat(faker.address.longitude()), parseFloat(faker.address.latitude())],
-  },
-  password: bcrypt.hashSync(password, 8),
-  _id: new ObjectID(faker.random.alphaNumeric(12)),
-  usersBlocked: [],
-  emailVerified: true,
-};
+const { initialUser, newUser, initalPassword, loggedUser } = require('./data');
 
 beforeAll(async () => {
   await connectDb();
@@ -39,29 +18,17 @@ afterAll(async () => {
   await disconnectDb();
 });
 
-test('should sign up a new user', async () => {
+test('should register a new user', async () => {
   await request(app)
     .post('/api/users')
-    .send({
-      username: faker.internet.userName(),
-      birthDate: faker.date.past(),
-      firstName: faker.name.firstName(),
-      lastName: faker.name.lastName(),
-      email: faker.internet.email(),
-      gender: 'male',
-      address: {
-        type: 'Point',
-        name: faker.address.streetAddress(),
-        coordinates: [parseFloat(faker.address.longitude()), parseFloat(faker.address.latitude())],
-      },
-      password: faker.internet.password(),
-    })
+    .send(newUser)
     .expect(201);
 });
 
 test('should login user', async () => {
-  await request(app)
+  const { body: user } = await request(app)
     .post('/api/users/login')
-    .send({ username: initialUser.username, password })
+    .send({ username: initialUser.username, initalPassword })
     .expect(200);
+  expect(user).toMatchObject(loggedUser);
 });
