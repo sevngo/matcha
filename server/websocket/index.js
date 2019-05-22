15227c54862ae976f486faf3ea7b4aa902305b99
Steven Ngo <sevngo@gmail.com>
2019-05-22 15:39:44 +0200

    rename emitToFriends to emitToFriendsConnected

--- server/websocket/index.js
+++ server/websocket/index.js
@@ -1,10 +1,15 @@
-const { addUser, removeUserById, removeUserBySocketId, emitToFriends } = require('./users');
+const {
+  addUser,
+  removeUserById,
+  removeUserBySocketId,
+  emitToFriendsConnected,
+} = require('./users');
 
 const socketEvents = io => {
   io.on('connection', socket => {
     socket.on('login', user => {
       addUser(user._id, socket.id);
-      emitToFriends(io, user.friends, 'friendConnected');
+      emitToFriendsConnected(io, user.friends, 'friendConnected');
     });
     socket.on('logout', removeUserById);
     socket.on('disconnect', () => removeUserBySocketId(socket.id));
