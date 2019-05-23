30df2c5147ca54d2de27215d1e03f1c330a315dc
Steven Ngo <sngo@europ-assistance.com>
2019-05-23 15:12:58 +0200

    renames socket events

--- client/index.js
+++ client/index.js
@@ -18,8 +18,8 @@ import io from 'socket.io-client';
 export const socket = io('http://localhost:8080');
 
 socket.on('friendLogged', user => console.log('friendLogged', user));
-socket.on('userLiked', user => console.log('userLiked', user));
-socket.on('userBlocked', user => console.log('userBlocked', user));
+socket.on('gotLiked', user => console.log('userLiked', user));
+socket.on('gotBlocked', user => console.log('userBlocked', user));
 
 addLocaleData([...localeEn, ...localeFr]);
 
