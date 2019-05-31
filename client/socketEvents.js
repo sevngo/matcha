d6765ca48d817ed0bfe331f2043f5c424cc97f43
Steven Ngo <sngo@europ-assistance.com>
2019-05-31 12:31:21 +0200

    dipatch ADD_NOTIFICATION for all socket events

--- client/socketEvents.js
+++ client/socketEvents.js
@@ -4,28 +4,42 @@ import { GOT_FRIENDED, GOT_UNDFRIENDED, ADD_NOTIFICATION } from './actions';
 
 const socketEvents = socket => {
   socket.on('friendLogged', notification => {
-    console.log('friendLogged', notification);
-    store.dispatch({ type: ADD_NOTIFICATION, notification });
+    store.dispatch({
+      type: ADD_NOTIFICATION,
+      notification: { ...notification, messageId: 'friendLogged' },
+    });
   });
   socket.on('gotLiked', notification => {
-    console.log('gotLiked', notification);
-    store.dispatch({ type: ADD_NOTIFICATION, notification });
+    store.dispatch({
+      type: ADD_NOTIFICATION,
+      notification: { ...notification, messageId: 'gotLiked' },
+    });
   });
   socket.on('gotBlocked', notification => {
-    console.log('gotBlocked', notification);
-    store.dispatch({ type: ADD_NOTIFICATION, notification });
+    store.dispatch({
+      type: ADD_NOTIFICATION,
+      notification: { ...notification, messageId: 'gotBlocked' },
+    });
   });
   socket.on('gotFriended', notification => {
     store.dispatch({ type: GOT_FRIENDED, user: notification.user });
-    store.dispatch({ type: ADD_NOTIFICATION, notification });
+    store.dispatch({
+      type: ADD_NOTIFICATION,
+      notification: { ...notification, messageId: 'gotFriended' },
+    });
   });
   socket.on('gotUnfriended', notification => {
     store.dispatch({ type: GOT_UNDFRIENDED, _id: path(['user', '_id'])(notification) });
-    store.dispatch({ type: ADD_NOTIFICATION, notification });
+    store.dispatch({
+      type: ADD_NOTIFICATION,
+      notification: { ...notification, messageId: 'gotUnfriended' },
+    });
   });
   socket.on('gotVisited', notification => {
-    console.log('gotVisited', notification);
-    store.dispatch({ type: ADD_NOTIFICATION, notification });
+    store.dispatch({
+      type: ADD_NOTIFICATION,
+      notification: { ...notification, messageId: 'gotVisited' },
+    });
   });
 };
 
