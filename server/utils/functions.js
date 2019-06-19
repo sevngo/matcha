600250b7ebcae1f83777e5a3bcec619a28d8e5cf
Steven Ngo <sevngo@gmail.com>
2019-06-19 15:35:01 +0200

    enhance getAppUrl function

--- server/utils/functions.js
+++ server/utils/functions.js
@@ -4,12 +4,11 @@ const { NODE_ENV, DEVSERVER_PORT } = require('./constants');
 
 exports.defaultToNull = defaultTo(null);
 
-exports.getAppUrl = req => {
-  const { protocol, hostname } = req;
+exports.getAppUrl = (protocol, hostname, host) => {
   const fullUrl =
     NODE_ENV === 'development'
       ? `${protocol}://${hostname}:${DEVSERVER_PORT}`
-      : `${protocol}://${req.get('host')}`;
+      : `${protocol}://${host}`;
   return fullUrl;
 };
 
