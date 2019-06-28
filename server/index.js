fd2ee5170c6e2f7096136f4063edafbdf198fc5f
Steven Ngo <sevngo@gmail.com>
2019-06-28 11:37:13 +0200

    server: seperate app from http index

--- server/index.js
+++ server/index.js
@@ -1,40 +1,19 @@
 const http = require('http');
 const socketIo = require('socket.io');
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
 const { connectDb } = require('./database');
+const app = require('./app');
 const { PORT } = require('./utils/constants');
-const usersRouter = require('./users/router');
-const { NODE_ENV, DEVELOPMENT, PRODUCTION, TEST } = require('./utils/constants');
 const socketEvents = require('./websocket');
-
-const app = express();
-
-app.use(express.json());
-
-if (NODE_ENV === DEVELOPMENT) app.use(morgan('dev'));
-
-app.use('/api/users', usersRouter);
-
-if (NODE_ENV === PRODUCTION) {
-  app.use(express.static('dist'));
-  app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'dist', 'index.html')));
-}
 
 const server = http.createServer(app);
 const io = socketIo(server);
 socketEvents(io);
 
-if (NODE_ENV !== TEST) {
-  (async () => {
-    try {
-      await connectDb();
-      await server.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
-    } catch (err) {
-      console.log(err); // eslint-disable-line no-console
-    }
-  })();
-}
-
-module.exports = app;
+(async () => {
+  try {
+    await connectDb();
+    await server.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
+  } catch (err) {
+    console.log(err); // eslint-disable-line no-console
+  }
+})();
