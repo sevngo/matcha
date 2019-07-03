d2b01449899b64204726b2eec175af637be90988
Steven Ngo <sevngo@gmail.com>
2019-07-03 11:07:52 +0200

    add travis build

--- server/database.js
+++ server/database.js
@@ -10,7 +10,7 @@ exports.connectDb = async () => {
   client = await MongoClient.connect(MONGODB_URL, { useNewUrlParser: true });
   db = client.db(DATABASE_NAME);
   console.log('Database connected'); // eslint-disable-line no-console
-  return await usersModel(db);
+  await usersModel(db);
 };
 
 exports.disconnectDb = () => client.close();
