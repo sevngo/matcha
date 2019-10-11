e7ba0c6b394b6413df7e7d45e8dcb16365b0acb9
Steven Ngo <sevngo@gmail.com>
2019-10-11 20:52:57 +0200

    update README with env example

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -8,7 +8,7 @@ const {
   SENDGRID_API_KEY,
 } = process.env;
 
-exports.JWT_SECRET = JWT_SECRET;
+exports.JWT_SECRET = JWT_SECRET || 'mySecret';
 exports.DEVSERVER_PORT = DEVSERVER_PORT;
 exports.NODE_ENV = NODE_ENV;
 exports.MONGODB_URL = MONGODB_URL;
