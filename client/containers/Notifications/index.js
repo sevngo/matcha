3224538139d1ff13bceb41185d94b0950de8977d
Steven Ngo <sevngo@gmail.com>
2019-10-17 23:20:04 +0200

    fix notifications

--- client/containers/Notifications/index.js
+++ client/containers/Notifications/index.js
@@ -1,5 +1,5 @@
 import React, { useState, Fragment } from 'react';
-import { path, length, isEmpty } from 'ramda';
+import { path, length } from 'ramda';
 import { FormattedMessage } from 'react-intl';
 import { Typography, Grid, Icon, Divider, Badge, IconButton } from '@material-ui/core';
 import Popover from '../../components/Popover';
@@ -9,10 +9,9 @@ import messages from './messages';
 
 const Notifications = () => {
   const classes = useStyles();
-  const { removeNotification, notifications } = useConnect();
+  const { removeNotification, notifications = [] } = useConnect();
   const [anchorEl, handlePopover] = useState();
-  const hasNotifications = isEmpty(notifications);
-  const notificationsLength = length(notifications);
+  const notificationsLength = length(notifications) || 0;
   return (
     <Fragment>
       <IconButton color="inherit" onClick={e => handlePopover(e.currentTarget)}>
@@ -20,32 +19,30 @@ const Notifications = () => {
           <Icon>notifications_icon</Icon>
         </Badge>
       </IconButton>
-      {!hasNotifications && (
-        <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => handlePopover()}>
-          <Grid container direction="column" spacing={2}>
-            {notifications.map((notification, index) => (
-              <Fragment key={notification._id}>
-                <Grid container item justify="space-between" alignItems="center">
-                  <Typography variant="subtitle1">
-                    <FormattedMessage
-                      {...messages[notification.messageId]}
-                      values={{ username: path(['user', 'username'])(notification) }}
-                    />
-                  </Typography>
-                  <Icon
-                    onClick={() => removeNotification(notification._id)}
-                    color="inherit"
-                    className={classes.ml1}
-                  >
-                    delete
-                  </Icon>
-                </Grid>
-                {notificationsLength - 1 !== index && <Divider />}
-              </Fragment>
-            ))}
-          </Grid>
-        </Popover>
-      )}
+      <Popover open={Boolean(anchorEl)} anchorEl={anchorEl} onClose={() => handlePopover()}>
+        <Grid container direction="column" spacing={2}>
+          {notifications.map((notification, index) => (
+            <Fragment key={notification._id}>
+              <Grid container item justify="space-between" alignItems="center">
+                <Typography variant="subtitle1">
+                  <FormattedMessage
+                    {...messages[notification.messageId]}
+                    values={{ username: path(['user', 'username'])(notification) }}
+                  />
+                </Typography>
+                <Icon
+                  onClick={() => removeNotification(notification._id)}
+                  color="inherit"
+                  className={classes.ml1}
+                >
+                  delete
+                </Icon>
+              </Grid>
+              {notificationsLength - 1 !== index && <Divider />}
+            </Fragment>
+          ))}
+        </Grid>
+      </Popover>
     </Fragment>
   );
 };
