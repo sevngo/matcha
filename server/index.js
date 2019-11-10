d10ee18d81a7503923f9c3acbaee889f982156b9
Steven Ngo <sngo@MacBook-Pro-de-Steven.local>
2019-11-10 14:45:40 +0100

    move configs to package

--- server/index.js
+++ server/index.js
@@ -1,18 +1,15 @@
-const http = require('http');
 const socketIo = require('socket.io');
 const { connectDb } = require('./database');
 const app = require('./app');
 const { PORT } = require('./utils/constants');
 const socketEvents = require('./websocket');
 
-const server = http.createServer(app);
-const io = socketIo(server, { pingTimeout: 60000 });
-socketEvents(io);
-
 (async () => {
   try {
     await connectDb();
-    await server.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
+    const server = await app.listen(PORT, () => console.log(`Server listening on port ${PORT}`)); // eslint-disable-line no-console
+    const io = socketIo(server, { pingTimeout: 60000 });
+    socketEvents(io);
   } catch (err) {
     console.log(err); // eslint-disable-line no-console
   }
