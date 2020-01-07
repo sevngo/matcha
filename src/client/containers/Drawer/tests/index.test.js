da74485f2ee2bdab4e1741dde402a4cf4fd31e60
Steven Ngo <sevngo@gmail.com>
2020-01-07 11:40:44 +0100

    add snapshot tests in containers components

---
+++ src/client/containers/Drawer/tests/index.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import TestProvider from '../../../components/TestProvider';
+import Component from '../index';
+
+describe('Drawer', () => {
+  it('should match snapshot', () => {
+    const { container } = render(
+      <TestProvider>
+        <Component />
+      </TestProvider>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
