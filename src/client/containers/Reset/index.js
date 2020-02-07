c871f50e4429acbea27945f3d3c8284356a63d3d
Steven Ngo <sngo@europ-assistance.com>
2020-02-07 15:06:20 +0100

    Use react-router 5 best practices

--- src/client/containers/Reset/index.js
+++ src/client/containers/Reset/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { useParams } from 'react-router';
 import Paper from '../../components/Paper';
 import UserForm from '../../components/UserForm';
 import { useAuth } from '../../hooks';
 import useStyles from './styles';
 
-const Reset = ({
-  match: {
-    params: { token },
-  },
-}) => {
+const Reset = () => {
+  const { token } = useParams();
   const { updateUser } = useAuth();
   const classes = useStyles();
   return (
--- src/client/containers/User/index.js
+++ src/client/containers/User/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
+import { useParams } from 'react-router';
 import MyUser from '../MyUser';
 import OtherUser from '../OtherUser';
 import { useAuth } from '../../hooks';
 
-const User = ({
-  match: {
-    params: { id },
-  },
-}) => {
+const User = () => {
+  const { id } = useParams();
   const { auth } = useAuth();
   if (id === auth._id) return <MyUser />;
   return <OtherUser id={id} />;
