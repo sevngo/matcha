870599039fc1e4a165e6921b7be6645fa1a960b0
Steven Ngo <sngo@MacBook-Pro-de-Steven.local>
2020-05-06 18:41:48 +0200

    Add docker-compose file

--- src/server/websocket/users.js
+++ src/server/websocket/users.js
@@ -8,46 +8,34 @@ exports.addUserSocketId = async (_id, socketId) => {
   await Users.findOneAndUpdate({ _id: ObjectID(_id) }, { $set: { socketId } });
 };
 
-exports.removeUserSocketId = async socketId => {
-  try {
-    const Users = getUsers();
-    await Users.findOneAndUpdate({ socketId }, { $unset: { socketId: '' } });
-  } catch (e) {
-    console.log(e);
-  }
+exports.removeUserSocketId = async (socketId) => {
+  const Users = getUsers();
+  await Users.findOneAndUpdate({ socketId }, { $unset: { socketId: '' } });
 };
 
 exports.emitToFriendsConnected = async (io, data, friends, eventName) => {
-  try {
-    const Users = getUsers();
-    const friendsIds = map(id => ObjectID(id))(getIds(friends));
-    const friendsConnected = await Users.aggregate()
-      .match({ _id: { $in: friendsIds } })
-      .match({ sockerId: { $exists: true } })
-      .toArray();
-    const friendsSocketIds = getSocketIds(friendsConnected);
-    friendsSocketIds.forEach(friendSocketId => {
-      const notification = createNotification(data);
-      io.to(friendSocketId).emit(eventName, notification);
-    });
-  } catch (e) {
-    console.log(e);
-  }
+  const Users = getUsers();
+  const friendsIds = map((id) => ObjectID(id))(getIds(friends));
+  const friendsConnected = await Users.aggregate()
+    .match({ _id: { $in: friendsIds } })
+    .match({ sockerId: { $exists: true } })
+    .toArray();
+  const friendsSocketIds = getSocketIds(friendsConnected);
+  friendsSocketIds.forEach((friendSocketId) => {
+    const notification = createNotification(data);
+    io.to(friendSocketId).emit(eventName, notification);
+  });
 };
 
 exports.emitToUserConnected = async (io, data, receiverId, eventName) => {
-  try {
-    const _id = ObjectID(receiverId);
-    const Users = getUsers();
-    const [userConnected] = await Users.aggregate()
-      .match({ _id })
-      .match({ sockerId: { $exists: true } })
-      .toArray();
-    if (userConnected) {
-      const notification = createNotification(data);
-      io.to(userConnected.socketId).emit(eventName, notification);
-    }
-  } catch (e) {
-    console.log(e);
+  const _id = ObjectID(receiverId);
+  const Users = getUsers();
+  const [userConnected] = await Users.aggregate()
+    .match({ _id })
+    .match({ sockerId: { $exists: true } })
+    .toArray();
+  if (userConnected) {
+    const notification = createNotification(data);
+    io.to(userConnected.socketId).emit(eventName, notification);
   }
 };
