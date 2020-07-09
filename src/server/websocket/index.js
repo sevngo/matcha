6235377d3eafa1683195917d0c3d55b1d90b7257
Steven Ngo <sngo@europ-assistance.com>
2020-07-09 18:38:14 +0200

    Replace all console logs by pino

--- src/server/websocket/index.js
+++ src/server/websocket/index.js
@@ -4,6 +4,7 @@ const {
   emitToFriendsConnected,
   emitToUserConnected,
 } = require('./users');
+const pino = require('../utils/logger');
 
 const socketEvents = (socket) => {
   socket.on('logged', async ({ friends, _id, username }) => {
@@ -16,64 +17,64 @@ const socketEvents = (socket) => {
           friends,
           'friendLogged'
         );
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('reLogged', (_id) => {
     try {
       addUserSocketId(_id, socket.id);
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('userLiked', ({ user, userLikedId }) => {
     try {
       emitToUserConnected(socket, user, userLikedId, 'gotLiked');
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('userFriended', ({ user, userLikedId }) => {
     try {
       emitToUserConnected(socket, user, userLikedId, 'gotFriended');
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('userBlocked', ({ user, userBlockedId }) => {
     try {
       emitToUserConnected(socket, user, userBlockedId, 'gotBlocked');
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('userUnfriended', ({ user, userBlockedId }) => {
     try {
       emitToUserConnected(socket, user, userBlockedId, 'gotUnfriended');
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('logout', () => {
     try {
       removeUserSocketId(socket.id);
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 
   socket.on('disconnect', () => {
     try {
       removeUserSocketId(socket.id);
-    } catch (e) {
-      console.log(e);
+    } catch (err) {
+      pino.logger.error(err.stack);
     }
   });
 };
