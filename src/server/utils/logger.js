1bf227a613589e9d8d1ede42ed62e5eb584e5f5d
Steven Ngo <sevngo@gmail.com>
2021-01-24 21:54:35 +0100

    install pino-pretty

--- src/server/utils/logger.js
+++ src/server/utils/logger.js
@@ -1,5 +1,10 @@
 const pino = require('pino-http');
+const { NODE_ENV, PRODUCTION } = require('./env');
 
-const logger = pino();
+const logger = pino({
+  prettyPrint: {
+    levelFirst: NODE_ENV !== PRODUCTION,
+  },
+});
 
 module.exports = logger;
