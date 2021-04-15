2ea345cdfc97b0904a51456f04d0f982002b8a5b
Steven Ngo <sevngo@gmail.com>
2021-04-15 18:48:24 +0200

    add socket io server tests

--- src/server/websocket/tests/index.test.js
+++ src/server/websocket/tests/index.test.js
@@ -1,35 +1,58 @@
-import ioClient from 'socket.io-client';
-import http from 'http';
-import { Server as io } from 'socket.io';
 import socketEvents from '..';
 
-const ioOptions = {
-  transports: ['websocket'],
-  forceNew: true,
-  reconnection: false,
-};
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const Client = require('socket.io-client');
 
-let socketClient;
-let httpServer;
-let ioServer;
+describe('Socket Io Server', () => {
+  let io, clientSocket1, clientSocket2;
 
-beforeAll((done) => {
-  httpServer = http.createServer();
-  ioServer = new io(httpServer);
-  httpServer.listen(0, done);
-});
+  beforeAll((done) => {
+    const httpServer = createServer();
+    io = new Server(httpServer);
+    httpServer.listen(() => {
+      const port = httpServer.address().port;
+      clientSocket1 = new Client(`http://localhost:${port}`);
+      clientSocket2 = new Client(`http://localhost:${port}`);
+      io.on('connection', socketEvents);
+      clientSocket1.on('connect', done);
+    });
+  });
 
-afterAll((done) => {
-  socketClient.disconnect();
-  httpServer.close(done);
-});
+  afterAll(() => {
+    io.close();
+    clientSocket1.close();
+    clientSocket2.close();
+  });
 
-describe('Websocket', () => {
-  it('should connect client to server socket io', (done) => {
-    const { port } = httpServer.address();
-    ioServer.on('connect', socketEvents);
+  const clientSocket1ID = '60084a46203c4e342b13114c';
+  const user = { _id: clientSocket1ID, username: 'username' };
 
-    socketClient = ioClient(`http://localhost:${port}`, ioOptions);
-    socketClient.on('connect', done);
+  test('should get gotLiked event', (done) => {
+    clientSocket1.on('gotLiked', (notification) => {
+      clientSocket1.emit('logout', clientSocket1ID);
+      expect(notification.user).toMatchObject(user);
+      done();
+    });
+    clientSocket1.emit('logged', clientSocket1ID);
+    clientSocket2.emit('userLiked', user, clientSocket1ID);
+  });
+  test('should get gotFriended event', (done) => {
+    clientSocket1.on('gotFriended', (notification) => {
+      clientSocket1.emit('logout', clientSocket1ID);
+      expect(notification.user).toMatchObject(user);
+      done();
+    });
+    clientSocket1.emit('reLogged', clientSocket1ID);
+    clientSocket2.emit('userFriended', user, clientSocket1ID);
+  });
+  test('should get gotUnfriended event', (done) => {
+    clientSocket1.on('gotUnfriended', (notification) => {
+      clientSocket1.emit('logout', clientSocket1ID);
+      expect(notification.user).toMatchObject(user);
+      done();
+    });
+    clientSocket1.emit('logged', clientSocket1ID);
+    clientSocket2.emit('userUnfriended', user, clientSocket1ID);
   });
 });
