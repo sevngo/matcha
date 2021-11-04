d92c76c32030ce13ed1206b352e481a1b238ff6e
Steven Ngo <sevngo@gmail.com>
2021-11-04 20:29:34 +0100

    update pino

--- src/server/utils/logger.js
+++ src/server/utils/logger.js
@@ -1,13 +1,16 @@
 import pino from 'pino-http';
 
 const logger = pino({
-  prettyPrint: {
-    levelFirst: true,
-    translateTime: 'HH:MM:ss.l o',
-    messageKey: 'msg',
-    messageFormat: '{msg} {res.statusCode} {req.method} {req.url}',
-    ignore: 'pid,hostname',
-    hideObject: true,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      levelFirst: true,
+      translateTime: 'HH:MM:ss.l o',
+      messageKey: 'msg',
+      messageFormat: '{msg} {res.statusCode} {req.method} {req.url}',
+      ignore: 'pid,hostname',
+      hideObject: true,
+    },
   },
 });
 
