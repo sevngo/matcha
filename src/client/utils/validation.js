7383d991827340d492f6476b28a22ac37c107069
Steven Ngo <sevngo@gmail.com>
2022-07-23 11:37:52 +0200

    change email regex

--- src/client/utils/validation.js
+++ src/client/utils/validation.js
@@ -1,6 +1,12 @@
 import { trim } from 'ramda';
 
 export const isEmail = (value) =>
-  !/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ? 'invalid' : null;
+  value
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )
+    ? null
+    : 'invalid';
 
 export const isTrimmed = (value) => (value !== trim(value) ? 'invalid' : null);
